ff26576e25e2120058e0c42a1f230a114a3ef374
mgmira@wiremind.fr <mgmira@wiremind.fr>
2018-06-05 13:15:30 +0200

    log errors

--- celery_run/command.py
+++ celery_run/command.py
@@ -10,9 +10,15 @@ class CeleryRun(Command):
           'task', help='Task to run',
         ),
         parser.add_argument(
-          'task_kwargs', help='Kwargs to pass to task', type=json.loads
+          'task_kwargs', help='Kwargs to pass to task'
         )
 
     def run(self, task=None, task_kwargs=None, **kwargs):
+        try:
+            task_kwargs = json.loads(task_kwargs)
+        except ValueError:
+            print "%s was not parsed as json" % task_kwargs
+            return
         print 'Running %s with kwargs %s' % (task, task_kwargs)
         self.app.tasks[task](**task_kwargs)
+        print 'Task %s successfully ran'
